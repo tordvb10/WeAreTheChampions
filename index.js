da5abeb5a6e80f38d075ce706e5820d4ac625bae
tordvb10 <tordvb10.kodehode@gmail.com>
2023-11-20 14:26:51 +0100

    2011231426

--- index.js
+++ index.js
@@ -17,3 +17,31 @@ const db = getDatabase(app);
 const chatListInDB = ref(db, "chatList");
 
 const inputFieldEl = document.querySelector("textarea");
+focustextarea();
+const submitted = document.querySelector("button");
+const uls = document.getElementById("chat");
+
+submitted.addEventListener("click", function () {
+  const messageToSubmit = inputFieldEl.value;
+  let nyli = document.createElement("li");
+  nyli.innerText = messageToSubmit;
+  uls.appendChild(nyli);
+  inputFieldEl.value = "";
+});
+
+var input = document.getElementById("myInput");
+inputFieldEl.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    submitted.click();
+    focustextarea();
+  }
+});
+
+function slettchat() {
+  console.log("slett");
+}
+
+function focustextarea() {
+  inputFieldEl.focus();
+}
